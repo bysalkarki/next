18507ec241799e5e2d0aee2c1ab270c61c2cad54
bishal <bishal@bookmundi.com>
2024-07-12 11:31:05 +0545

    remove brands

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -277,33 +277,6 @@ export default function Home() {
 
         </section>
 
-        <section className="py-10 md:py-16">
-
-            <div className="container max-w-screen-xl mx-auto px-4">
-
-                <h1 className="font-medium text-gray-700 text-3xl md:text-4xl mb-5">Brands</h1>
-
-                <p className="font-normal text-gray-500 text-xs md:text-base mb-10 md:mb-20">Below is a summary of
-                    the places I studied</p>
-
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                    <Image src="/image/brand-1.png" alt="Image" height={100} width={100}/>
-
-                    <Image src="/image/brand-2.png" alt="Image" height={100} width={100}/>
-
-                    <Image src="/image/brand-3.png" alt="Image" height={100} width={100}/>
-
-                    <Image src="/image/brand-4.png" alt="Image" height={100} width={100}/>
-
-                    <Image src="/image/brand-5.png" alt="Image" height={100} width={100}/>
-
-                    <Image src="/image/brand-6.png" alt="Image" height={100} width={100}/>
-                </div>
-
-            </div>
-
-        </section>
-
         <footer className="py-10 md:py-16 mb-20 md:mb-40 lg::mb-52">
 
             <div className="container max-w-screen-xl mx-auto px-4">
