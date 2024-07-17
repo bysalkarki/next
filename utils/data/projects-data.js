2b823638c03646d9936c806d1fabb85894b38d23
bishal <bishal@bookmundi.com>
2024-07-17 14:05:38 +0545

    main

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,42 +1,53 @@
-import ayla from '/public/image/ayla.jpg';
-import crefin from '/public/image/crefin.jpg';
-import realEstate from '/public/image/real-estate.jpg';
-import travel from '/public/image/travel.jpg';
+import ayla from "/public/image/ayla.jpg";
+import crefin from "/public/image/crefin.jpg";
+import realEstate from "/public/image/real-estate.jpg";
+import travel from "/public/image/travel.jpg";
 
 export const projectsData = [
-   
-    {
-        id: 1,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
-        role: 'Full Stack Developer',
-        code: '',
-        demo: '',
-        image: travel,
-    },
-    {
-        id: 2,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
-        code: '',
-        role: 'Full Stack Developer',
-        demo: '',
-        image: realEstate,
-    },
-    {
-        id: 3,
-        name: 'Newsroom Management',
-        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
-        code: '',
-        demo: '',
-        image: ayla,
-        role: 'Full Stack Developer',
-    }
+  {
+    id: 1,
+    name: "Tour and Travel Aggregator Site - Bookmundi",
+    description:
+      "I contributed to the development of Bookmundi, a comprehensive tour and travel aggregator site. My work involved backend development, including booking functionalities and chat features. I utilized a range of technologies to build and integrate features, ensuring smooth user experiences and efficient data management. The project required detailed documentation, coding, and integration into the existing product.",
+    tools: [
+      "NestJS",
+      "Docker",
+      "MySQL",
+      "MongoDB",
+      "Redis",
+      "PHP",
+      "Node.js",
+      "Stripe",
+      "Cronjob'",
+    ],
+    role: "Software Developer",
+    code: "",
+    demo: "",
+    image: travel,
+  },
+  {
+    id: 2,
+    name: "Cargo Management System",
+    description:
+      "My team developed a comprehensive Cargo Management System using Laravel for the backend and MySQL for the database. We implemented Redis for caching and session management. The system includes features such as real-time tracking, inventory management, and automated notifications. We also integrated role-based access control, reporting tools, and an intuitive user interface.",
+    tools: ["Laravel", "MySQL", "Redis", "HTML", "CSS", "JavaScript'"],
+    code: "",
+    role: "Full Stack Developer",
+    demo: "",
+    image: realEstate,
+  },
+  {
+    id: 3,
+    name: "Newspaper Management",
+    description:
+      "My team and I developed a newspaper management dashboard application. As a backend developer, I was responsible for integrating the frontend template using Laravel and developing the API. I handled all the core functionalities of the application, ensuring seamless communication between the frontend and backend. We utilized Laravel's robust features to implement role-based access control, user authentication, and efficient data management. The API was designed to be secure and scalable, supporting all necessary CRUD operations and integrating third-party services as required.",
+    tools: ["laravel", "livewire", "mongoDb", "Mysql", "Git'"],
+    code: "",
+    demo: "",
+    image: ayla,
+    role: "Full Stack Developer",
+  },
 ];
-
 
 // Do not remove any property.
 // Leave it blank instead as shown below
